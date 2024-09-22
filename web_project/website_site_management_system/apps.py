c7538e7509f828cb2f57bf9d682d790489e2099c
Wang Linhao <wilkinsddvd@qfnu.edu.cn>
2024-09-22 15:59:40 +0800

    python_Django框架

---
+++ web_project/website_site_management_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebsiteSiteManagementSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'website_site_management_system'
