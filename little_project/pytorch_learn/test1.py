3a607ee7f8e3540652caa4b7fd06a22419e85f06
Wang Linhao <wilkinsddvd@qfnu.edu.cn>
2024-10-24 11:37:45 +0800

    Organize and clear some messy files

--- little_project/pytorch_learn/test1.py
+++ little_project/pytorch_learn/test1.py
@@ -1,4 +1,19 @@
+# 1. 导入必要的库
 import torch
+import numpy as np
 
-a = torch.cuda.is_available()
-print(a)
+# 2. 随机生成矩阵A和向量b
+# 假设我们想要一个3x3的方程组
+A = np.random.rand(3, 3)
+b = np.random.rand(3, 1)
+
+# 3. 使用PyTorch创建对应的张量
+A_torch = torch.tensor(A, dtype=torch.float32)
+b_torch = torch.tensor(b, dtype=torch.float32)
+
+# 4. 使用PyTorch的线性代数功能求解线性方程组
+x_torch = torch.linalg.solve(A_torch, b_torch)
+
+# 打印结果
+print("The solution vector x is:")
+print(x_torch)
--- web_project/website_site_management_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebsiteSiteManagementSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website_site_management_system'
